e480950944706ec9fad6c5b5fbbb50497f360007
Jason Lin <157414523@qq.com>
2018-09-24 11:02:26 +0800

    no message

--- application/main/forms.py
+++ application/main/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
+import os
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, EqualTo, ValidationError
 
 class HomeSearchForm(FlaskForm):
     search = StringField('Search', validators=[DataRequired()]
-                            , render_kw={"placeholder": "Search this site"})
+                            , render_kw={"placeholder": os.environ.get('MySQL')})
     submit = SubmitField('Search')
