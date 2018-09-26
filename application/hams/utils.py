ca24f34fd388f77094042b9bc4ba862962d895d9
Jason Lin <157414523@qq.com>
2018-09-26 14:27:47 +0800

    no message

--- application/hams/utils.py
+++ application/hams/utils.py
@@ -21,47 +21,5 @@ def save_datafile(form_datafile, user):
         dir_n = user + '/unprocessed/test/User Spectra/' + fn
 
     s3.put_object(Bucket=bucket_name, Key=dir_n, Body=form_datafile.data)
-
-    """ if form_datafile.label == 'Target':
-        new_path = (current_app.root_path +
-                '/static/data/' + user + 
-                '/unprocessed/test/Target/')
-    elif form_datafile.label == 'HRMS Data (Low Energy)':
-        new_path = (current_app.root_path +
-                '/static/data/' + user + 
-                '/unprocessed/test/Low Energy/')
-    elif form_datafile.label == 'HRMS Data (High Energy)':
-        new_path = (current_app.root_path +
-                '/static/data/' + user + 
-                '/unprocessed/test/High Energy/')
-    else:
-        new_path = (current_app.root_path +
-                '/static/data/' + user + 
-                '/unprocessed/test/User Spectra/')
     
-    if not os.path.exists(new_path):
-        os.makedirs(new_path)
-
-    if form_datafile.label == 'Target':
-        file_path = os.path.join(current_app.root_path,
-                            'static/data/', user,
-                            'unprocessed/test/Target/', fn)
-
-    elif form_datafile.label == 'HRMS Data (Low Energy)':
-        file_path = os.path.join(current_app.root_path,
-                            'static/data/', user,
-                            'unprocessed/test/Low Energy/', fn)
-
-    elif form_datafile.label == 'HRMS Data (High Energy)':
-        file_path = os.path.join(current_app.root_path,
-                            'static/data/', user,
-                            'unprocessed/test/High Energy/', fn)
-
-    else:
-        file_path = os.path.join(current_app.root_path,
-                            'static/data/', user,
-                            'unprocessed/test/User Spectra/', fn)
-
-    form_datafile.data.save(file_path) """
-
     return fn
