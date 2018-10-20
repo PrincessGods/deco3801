a1b88f2635863ee56e020c98ac2a75b918eee20f
Jason Lin <157414523@qq.com>
2018-10-21 02:43:53 +0800

    no message

--- application/hams/utils.py
+++ application/hams/utils.py
@@ -184,6 +184,6 @@ def RemoveFromEBS(user):
 def DownloadFromS3ToLocal(user, JobID):
     s3 = boto3.client('s3')
     key = user + '/' + JobID + '/processed/ULSA/ULSA_01540052918.43.csv'
-    url = s3.list_objects_v2(Bucket='deco3801mars')
+    url = s3.get_object_acl(Bucket='deco3801mars', Key=key)
     return url
     #s3.meta.client.download_file('deco3801mars', key, 'application/download.csv')
