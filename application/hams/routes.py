7484c7847fe873be7b37623b0b94bdce723254e9
Jason Lin <157414523@qq.com>
2018-10-21 11:04:57 +0800

    no message

--- application/hams/routes.py
+++ application/hams/routes.py
@@ -234,5 +234,5 @@ def saveJob(chosenMethod):
 def download(jobid):
     url = DownloadFromS3ToLocal(current_user.user_email, jobid)
     print(url)
-    return redirect(url)
-    return redirect(url_for('users.profile'))
+    redirect(url)
+    return render_template('viewJobs.html',icon = user_icon, title = "Review My Uploads", mySample_info = mySample_info)
