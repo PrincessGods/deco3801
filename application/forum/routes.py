9c895815f8cee197f9722f163e2d00417c9fc59b
Jason Lin <157414523@qq.com>
2018-10-22 03:25:32 +0800

    no message

--- application/forum/routes.py
+++ application/forum/routes.py
@@ -16,6 +16,17 @@ def viewpost():
     form = PostForm()
     user_icon = getUserIcon()
     posts = Post.query.all()
+    if form.validate_on_submit():
+        post = Post(
+            title = form.title.data,
+            contents = form.content.data,
+            owner = current_user.id
+        )
+
+        db.session.add(post)
+        db.session.commit()
+
+        return redirect(url_for('forum.viewpost'))
     return render_template('forum.html', title='Forum', 
                             form=form, icon = user_icon, 
                             posts = posts)
